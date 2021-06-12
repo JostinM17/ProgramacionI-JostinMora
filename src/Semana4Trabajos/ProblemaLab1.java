be6217c5d8b70044b36b00234ce49e3491a8bdcc
JostinM17 <moragamboa96@gmail.com>
2021-06-11 20:59:30 -0600

    Lab1

---
+++ src/Semana4Trabajos/ProblemaLab1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Semana4Trabajos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jostin Mora
+ */
+public class ProblemaLab1 {
+    public static void main (String[] args){
+        
+        Scanner escaner = new Scanner(System.in);
+        System.out.println("Digite cuantos factores de riesgo: ");
+        int numriesgos;
+    
+        
+    }
+}
