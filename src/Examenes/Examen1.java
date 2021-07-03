83763556bd9ff281e84165c4a63cf30dd9db277c
JostinM17 <moragamboa96@gmail.com>
2021-07-02 18:06:15 -0600

    Examen1, primer commit

---
+++ src/Examenes/Examen1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examenes;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jostin Mora
+ */
+public class Examen1 {
+    public static void main(String[] args){
+        
+        String nombreDeEmpresa;
+        String provincia;
+        int cantTrabTotl;
+        int cantTrabCovid;
+        int montoProm;
+        boolean creditos;
+
+        Scanner escaner = new Scanner(System.in);
+        
+        while(true){
+            System.out.println("Menu Dinamico \n1)Digitar datos de la empresa \n2)Imprimir la informacion completade la empresa\n3)"
+                    + "Cantidad de personas por provincia\n4)Imprimir monto de prestamo y ajuste\n5");
+            
+        }
+    }
+    
+}
