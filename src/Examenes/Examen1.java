15864720a5388a627310d7ac83dd980e922b7dd9
JostinM17 <moragamboa96@gmail.com>
2021-07-02 18:41:36 -0600

    Examen1, segundo commit

--- src/Examenes/Examen1.java
+++ src/Examenes/Examen1.java
@@ -14,19 +14,63 @@ import java.util.Scanner;
 public class Examen1 {
     public static void main(String[] args){
         
-        String nombreDeEmpresa;
-        String provincia;
-        int cantTrabTotl;
-        int cantTrabCovid;
-        int montoProm;
-        boolean creditos;
+        String nombreDeEmpresa = "";
+        String provincia = "";
+        int cantTrabTotl = 0;
+        int cantTrabCovid = 0;
+        int montoProm = 0;
+        boolean creditos = false;
 
         Scanner escaner = new Scanner(System.in);
         
         while(true){
-            System.out.println("Menu Dinamico \n1)Digitar datos de la empresa \n2)Imprimir la informacion completade la empresa\n3)"
-                    + "Cantidad de personas por provincia\n4)Imprimir monto de prestamo y ajuste\n5");
+            System.out.println("Menu Dinamico \n1)Digitar datos de la empresa \n2)"
+                    + "Imprimir la informacion de la empresa\n3)"
+                    + "Cantidad de personas por provincia\n4)Imprimir monto de prestamo y ajuste\n5)"
+                    + "Estado inicial");
+            int opcion = escaner.nextInt();
             
+            switch(opcion){
+                
+                case 1:{
+                    System.out.println("Digite el nombre de la empresa");
+                    nombreDeEmpresa = escaner.next();
+                    
+                    System.out.println("Digite la provincia donde se ubica");
+                    provincia = escaner.next();
+                    
+                    System.out.println("Digite la cantidad de trabajadores totales");
+                    cantTrabTotl = escaner.nextInt();
+                    
+                    System.out.println("Digite la cantidad de trabajadores que han tenido COVID-19");
+                    cantTrabCovid = escaner.nextInt();
+                    
+                    System.out.println("Digite el ingreso promedio por mes");
+                    montoProm = escaner.nextInt();
+                    
+                    System.out.println("Digite si la empresa tiene creditos o no");
+                    creditos = escaner.nextBoolean();
+                    break;
+                }
+                case 2:{
+                    System.out.println("Informacion completa de la empresa: "
+                            +"\n "+nombreDeEmpresa+"\n "+provincia+"\n"+cantTrabTotl+"\n"+cantTrabCovid
+                            +"\n"+montoProm+"\n"+creditos+"\n");
+                    break;
+                }
+                case 3:{
+                    break;
+                }
+                case 4:{
+                    break;
+                }
+                case 5:{
+                    break;
+                }
+                case 6:{
+                    break;
+                }
+            }
         }
     }
     
